cccd796f9e57376c43e80d769a54ec560d709d6d
Natalie & cool greg <pair+nztzlie.scoin@devbootcamp.com>
2014-06-13 17:55:15 -0400

    lame

---
+++ models.rb
@@ -0,0 +1,49 @@
+require 'sqlite3'
+
+class Model
+  def initialize
+     @db = SQLite3::Database.open "voter.db"
+  end
+
+  def user_exists?(userdata)
+    @name = @db.execute("SELECT id, username FROM users WHERE username = ?", userdata)
+    p
+    @name.flatten!
+    !@name.empty?
+  end
+
+  def new_user(username)
+    @db.execute("INSERT INTO users(username) VALUES (?)", username)
+    puts "New User Created #{username}"
+    username
+  end
+
+  def login(username)
+    #connect to the db and authenticate username
+    #else creates a new user
+    #returns the username which is saved for the session
+    if user_exists?(username)
+      return username
+    else
+      new_user(username)
+      return username
+    end
+  end
+
+  def load_subjects
+    subjects = @db.execute("SELECT subject_name, username FROM subjects JOIN users ON (users.id = subjects.user_id)")
+  end
+
+  def add_subject(subject)
+    @db.execute("INSERT INTO subjects(subject_name, user_id) VALUES (?, ?)", subject, @name[0])
+    new_subject_id = @db.execute("SELECT MAX(id) FROM subjects")
+  end
+
+  def load_answers(subject_id)
+  answers = @db.execute("SELECT answer_text FROM answers JOIN subjects ON (subjects.id = answers.subject_id) WHERE subject_id = (?)", subject_id)
+  end
+
+  def add_answer(answer, subject)
+  @db.execute("INSERT INTO answers(answer_text, user_id, subject_id) VALUES (?, ?, ?)", answer, @name[0], subject)
+  end
+end
---
+++ view.rb
@@ -0,0 +1,77 @@
+class View
+
+def self.welcome
+  #get username from user (& password when implemented)
+  #returns username to controller
+  #send back to the login model via controller
+  #print ascii art and user name ('welcome back') or ('new user created')
+
+  puts "Welcome to DBC Voter!!"
+  puts "Where you can create subjects, answer them, and vote on the best answers!"
+  puts "What is your username?"
+  username = gets.chomp
+end
+
+def self.subjects_menu(subjects)
+  puts "#  Subject Title        Submitter"
+  subjects.each_with_index{|element, index|
+    print "#{index + 1}. #{element[0]} -- #{element[1]}"
+    puts
+  }
+end
+
+def self.subjects_menu_options
+  puts "Which number subject would you like to view?"
+  puts "Or type add to add a new subject"
+  x = gets.chomp
+end
+
+  #is sent list of subjects from model db
+  #looping interface
+  #shows list of subjects
+  #option to view answers (by line number)
+  #option to add subject
+
+  def self.new_subject
+    puts "Enter the subject you would like to add"
+    x = gets.chomp
+  end
+
+
+def self.view_answers(answers)
+puts "Answers"
+answers.each_with_index { |answer, index|
+  print "#{index + 1}. #{answer}"
+  puts
+  }
+#displays all the answers for chosen subject - ranked by number of votes
+#displays number of votes
+#option to add answer
+#option to vote
+end
+
+def self.answers_menu_options
+puts "Select an answer to vote up by number"
+puts "Or type add to add a new answer"
+x = gets.chomp
+end
+
+def self.new_answer
+puts "Write a new answer for this question"
+x = gets.chomp
+#takes a new answer from the user
+#submits to db
+end
+
+end
+#VIEWS
+# login / welcome
+###user page
+###password
+# view subjects
+#   view answers
+#     vote
+      ###comment
+#   add answer
+
+# add subjects
