ed67f4821e4f60229e3070fbe8bb862bc2edc26f
Tanner Welsh <tannerwelsh@gmail.com>
2014-06-16 14:24:19 -0400

    Housekeeping: - Move Controller to own file - Clean up comments - Minor refactoring in View

---
+++ controller.rb
@@ -0,0 +1,73 @@
+class Controller
+  #display welcome view
+  #receive the username from welcome view
+  #pass the username to login model
+
+  #receive authentication from login model either way
+  #display subjects_menu view
+  #receive option variable from subjects view and pass through to view answers or create subject
+  #   option 1 - view answers
+  #   option 2 - create subjects - takes input from user, creates subject in db
+  #   takes you to view answers for just created subject
+  #controller sends answers from db view answers
+
+  def initialize
+    @model = Model.new
+  end
+
+  def program_loop
+    View.welcome
+
+    @username = @model.login(View.get_input)
+
+    loop do
+      subject_array = subjects_sequence
+
+      break if subject_array == 'quit'
+
+      answers_sequence(subject_array) if subject_array.first != 0
+    end
+  end
+
+  def subjects_sequence
+    subjects_data = @model.load_subjects
+    subject_display = subjects_data[0]
+    subjects_by_index = @model.assign_id_indices(subjects_data[1])
+    View.subjects_menu(subject_display, @username)
+    View.subjects_menu_options
+    subject_choice = View.get_input
+    return 'quit' if subject_choice.downcase == 'quit'
+    if subject_choice.downcase == "add"
+      View.new_subject
+      subject_string = View.get_input
+      new_subject_id = @model.add_subject(subject_string) if !subject_string.empty?
+      return [new_subject_id, subject_string]
+    end
+    subject_choice = subject_choice.to_i
+    if subjects_by_index[subject_choice] != nil
+      subject_array = [subjects_by_index[subject_choice], subject_display.keys[subject_choice - 1]]
+    else
+      return [0]
+    end
+  end
+
+  def answers_sequence(subject_array)
+    subject_choice, subject_name = subject_array
+    loop do
+      answers_hash, votes_hash = @model.sort_answers(@model.load_answers(subject_choice))
+      answers_with_index = @model.assign_id_indices(answers_hash)
+      View.view_answers(votes_hash, subject_name)
+      View.answers_menu_options
+      answer_choice = View.get_input
+      return if answer_choice.downcase == 'back'
+      if answer_choice.downcase == 'add'
+        View.new_answer
+        answer_string = View.get_input
+        @model.add_answer(answer_string, subject_choice.to_i) if !answer_string.empty?
+      elsif answer_choice.length >= 1
+        answer_choice = answer_choice.to_i
+        @model.vote(answers_with_index[answer_choice], subject_choice) if answer_choice != 0 && answers_with_index[answer_choice] != nil
+      end
+    end
+  end
+end
--- view.rb
+++ view.rb
@@ -1,97 +1,76 @@
 class View
+  def self.get_input
+    gets.strip
+  end
 
-def self.welcome
-#get username from user (& password when implemented)
-#returns username to controller
-#send back to the login model via controller
-#print ascii art and user name ('welcome back') or ('new user created')
+  def self.welcome
+    #get username from user (& password when implemented)
+    #returns username to controller
+    #send back to the login model via controller
+    #print ascii art and user name ('welcome back') or ('new user created')
 
-  puts "Welcome to DBC Voter!!"
-  puts "Where you can create subjects, answer them, and vote on the best answers!"
-  puts "What is your username?"
-  username = gets.chomp
-  username.strip
-end
+    puts "Welcome to DBC Voter!!"
+    puts "Where you can create subjects, answer them, and vote on the best answers!"
+    puts "What is your username?"
+  end
 
-def self.subjects_menu(subjects, username)
-  system('clear')
-  puts "Welcome, #{username}"
-  puts "".ljust(2) + "#".ljust(2)+"|Submitter".ljust(14) + "Subject Title"
-  puts "--------------------------------------------------------------------------"
-  subjects.each_with_index{|(k,v), index|
-    print "".ljust(2) + "#{index + 1}".ljust(2)+"|#{v}".ljust(14)+"#{k}"
+  def self.subjects_menu(subjects, username)
+    clear_screen
+
+    puts "Welcome, #{username}"
+    puts "".ljust(2) + "#".ljust(2)+"|Submitter".ljust(14) + "Subject Title"
+    puts "--------------------------------------------------------------------------"
+    subjects.each_with_index{|(k,v), index|
+      print "".ljust(2) + "#{index + 1}".ljust(2)+"|#{v}".ljust(14)+"#{k}"
+      puts
+    }
+  end
+
+  def self.subjects_menu_options
+    puts "--------------------------------------------------------------------------"
     puts
-  }
-end
+    puts "Which number subject would you like to view?"
+    puts "Or type add to add a new subject"
+    puts "Or type quit to exit"
+  end
 
-def self.subjects_menu_options
-  puts "--------------------------------------------------------------------------"
-  puts
-  puts "Which number subject would you like to view?"
-  puts "Or type add to add a new subject"
-  puts "Or type quit to exit"
-  x = gets.chomp
-  x.strip
-end
-
-#is sent list of subjects from model db
-#looping interface
-#shows list of subjects
-#option to view answers (by line number)
-#option to add subject
-
-def self.new_subject
-  puts "--------------------------------------------------------------------------"
-  puts
-  puts "Enter the subject you would like to add"
-  x = gets.chomp
-  x.strip
-end
-
-
-def self.view_answers(answers, subject_name)
-  system('clear')
-  puts subject_name
-  puts "-------------------------------------------------------"
-  puts "#".ljust(3) + "|VTS|".ljust(7) + "Answer"
-  puts "-------------------------------------------------------"
-  answers.each_with_index { |(answer, votes), index|
-    print "#{index + 1}.".ljust(3) + "|".ljust(2) + "#{votes}".ljust(2)+ "|".ljust(3) + "#{answer}"
+  def self.new_subject
+    puts "--------------------------------------------------------------------------"
     puts
-  }
-  puts
-#displays all the answers for chosen subject - ranked by number of votes
-#displays number of votes
-#option to add answer
-#option to vote
+    puts "Enter the subject you would like to add"
+  end
+
+  def self.view_answers(answers, subject_name)
+    #displays all the answers for chosen subject - ranked by number of votes
+    #displays number of votes
+    #option to add answer
+    #option to vote
+    clear_screen
+
+    puts subject_name
+    puts "-------------------------------------------------------"
+    puts "#".ljust(3) + "|VTS|".ljust(7) + "Answer"
+    puts "-------------------------------------------------------"
+    answers.each_with_index { |(answer, votes), index|
+      print "#{index + 1}.".ljust(3) + "|".ljust(2) + "#{votes}".ljust(2)+ "|".ljust(3) + "#{answer}"
+      puts
+    }
+    puts
+  end
+
+  def self.answers_menu_options
+    puts "Select an answer to vote up by number"
+    puts "Or type add to add a new answer"
+    puts "Or type back to go back to Main"
+  end
+
+  def self.new_answer
+    #takes a new answer from the user
+    #submits to db
+    puts "Write a new answer for this question"
+  end
+
+  def self.clear_screen
+    system('clear')
+  end
 end
-
-def self.answers_menu_options
-  puts "Select an answer to vote up by number"
-  puts "Or type add to add a new answer"
-  puts "Or type back to go back to Main"
-  x = gets.chomp
-  x.strip
-end
-
-def self.new_answer
-  puts "Write a new answer for this question"
-  x = gets.chomp
-  x.strip
-#takes a new answer from the user
-#submits to db
-end
-
-
-end
-#VIEWS
-# login / welcome
-###user page
-###password
-# view subjects
-#   view answers
-#     vote
-  ###comment
-#   add answer
-
-# add subjects
